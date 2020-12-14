784b56dcd0518bfac0692076011a6737df7a49ea
Justas Medeisis <justas@trafi.com>
2020-12-14 18:07:19 +0200

    Flow wrapper for iOS

--- common/core/src/commonMain/kotlin/com/trafi/state/State.kt
+++ common/core/src/commonMain/kotlin/com/trafi/state/State.kt
@@ -1,8 +1,14 @@
 package com.trafi.state
 
+import io.ktor.utils.io.core.Closeable
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 interface State<out T : State<T, E>, E> {
     fun reduce(event: E): T
@@ -15,5 +21,23 @@ class StateMachine<T : State<T, E>, E>(initial: T) {
 
     fun transition(event: E) {
         _stateFlow.value = _stateFlow.value.reduce(event)
+    }
+}
+
+fun <T> Flow<T>.wrap(): CFlow<T> = CFlow(this)
+
+class CFlow<T>(private val origin: Flow<T>) : Flow<T> by origin {
+    fun watch(block: (T) -> Unit): Closeable {
+        val job = Job(/*ConferenceService.coroutineContext[Job]*/)
+
+        onEach {
+            block(it)
+        }.launchIn(CoroutineScope(Dispatchers.Default + job))
+
+        return object : Closeable {
+            override fun close() {
+                job.cancel()
+            }
+        }
     }
 }
