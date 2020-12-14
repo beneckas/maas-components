4a07337fa37b7ba5c691c8113f0abebe97ecfca8
Justas Medeisis <justas@trafi.com>
2020-12-14 16:15:26 +0200

    Simple StateMachine implementation.

--- common/core/src/commonMain/kotlin/com/trafi/state/State.kt
+++ common/core/src/commonMain/kotlin/com/trafi/state/State.kt
@@ -1,5 +1,17 @@
 package com.trafi.state
 
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
 interface State<out T : State<T, E>, in E> {
     fun reduce(event: E): T
+}
+
+class StateMachine<T : State<T, E>, E>(initial: T) {
+    private val _stateFlow = MutableStateFlow(initial)
+    val stateFlow: StateFlow<T> = _stateFlow
+
+    fun transition(event: E) {
+        _stateFlow.value = _stateFlow.value.reduce(event)
+    }
 }
