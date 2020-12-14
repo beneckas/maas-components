134e33fa9244277ef5b34ef13569ce63ba7417b5
Justas Medeisis <justas@trafi.com>
2020-12-14 18:05:22 +0200

    reduceFlow CounterState

--- common/core/src/commonMain/kotlin/com/trafi/state/CounterState.kt
+++ common/core/src/commonMain/kotlin/com/trafi/state/CounterState.kt
@@ -1,5 +1,9 @@
 package com.trafi.state
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
 enum class CounterEvent {
     Increment,
     Decrement,
@@ -9,5 +13,12 @@ data class CounterState(val count: Int) : State<CounterState, CounterEvent> {
     override fun reduce(event: CounterEvent): CounterState = when (event) {
         CounterEvent.Increment -> copy(count = count + 1)
         CounterEvent.Decrement -> copy(count = count - 1)
+    }
+
+    override fun reduceFlow(event: CounterEvent): Pair<CounterState, Flow<CounterEvent>> {
+        return reduce(event) to flow {
+            delay(500)
+            emit(CounterEvent.Increment)
+        }
     }
 }
